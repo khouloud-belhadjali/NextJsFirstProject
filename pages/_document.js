bdbb8742e7ed34a03fee5b7de028762cc34c82dc
khouloud-belhadjali <khouloud.belhadjali@enis.tn>
2024-08-15 00:54:17 +0200

    define document.js and use react portals

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// _document.js to define the general structure of our page
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+
+
+
+class MyDocument extends Document{
+  render(){
+    return(
+      <Html lang='en'>
+        <Head>
+          <body>
+            <Main/>
+            <NextScript />
+            <div id='notifications'></div>
+          </body>
+        </Head>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
